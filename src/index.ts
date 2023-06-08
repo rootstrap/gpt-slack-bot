e6f587f839deb2f809339b3c16c2a098f385a3c4
Juan Pablo Zuluaga <juan.zuluaga@rootstrap.com>
2023-06-08 07:49:50 -0500

    Remove ngrok dependency

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
 import 'dotenv/config';
-import * as ngrok from 'ngrok';
 import { slackApp } from './clients/slack.client';
 
-const isDev = process.env.STAGE === 'dev';
-
-slackApp.start(process.env.PORT || 3000).then(async () => {
-  if (isDev) {
-    const url = await ngrok.connect({ authtoken: process.env.NGROK_AUTH_TOKEN, addr: 3000 });
-    console.log(`Development url: ${url}`);
-  }
-});
+slackApp.start(process.env.PORT || 3000);
