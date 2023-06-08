9876865210bb20d5124ca7aa7997dcf457c2aa0a
Amaury <ricardo901130@gmail.com>
2023-06-08 15:15:49 -0300

    fix comments

--- src/constants/messages/system.prompts.ts
+++ src/constants/messages/system.prompts.ts
@@ -4,5 +4,5 @@ export const systemPrompts = {
   Answer in the same language that is the first message in the conversation.
   Try to be brief and straight to the important points.`,
   hi: `Answer`,
-  say: `You are a very serius persons, your name is Rs-GPT, generate a cordial message comming from you, not too long, with the following idea`,
+  say: `You are a very serius persons, your name is Rs-GPT, generate a cordial message comming from you,  not too long, the message is for an Slack Channel, you can use emojis if you want, with the following idea`,
 };
