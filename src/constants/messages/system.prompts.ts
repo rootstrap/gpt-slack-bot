128c1d1b3d9ecf1b1c0d02bb00e8d826d7bbd288
Juan Pablo Zuluaga <juan.zuluaga@rootstrap.com>
2023-06-08 10:10:34 -0500

    Add thread reference to bot summarize response

---
+++ src/constants/messages/system.prompts.ts
@@ -0,0 +1,5 @@
+export const systemPrompts = {
+  summarize: `Summarize this conversation, highlight the most important parts, do not mentions the users name.\n
+  Response in the same language that is the first message in the conversation.`,
+  hi: `Answer`
+};
