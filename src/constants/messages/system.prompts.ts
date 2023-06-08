02cb1d54c1d89c5cee8a5e6de6d11d712961c47a
Juan Pablo Zuluaga <juan.zuluaga@rootstrap.com>
2023-06-08 10:20:31 -0500

    Update src/constants/messages/system.prompts.ts
    Co-authored-by: Diego Suárez <diego.suarez@rootstrap.com>

--- src/constants/messages/system.prompts.ts
+++ src/constants/messages/system.prompts.ts
@@ -1,5 +1,6 @@
 export const systemPrompts = {
   summarize: `Summarize this conversation, highlight the most important parts, do not mentions the users name.\n
-  Response in the same language that is the first message in the conversation.`,
+  Answer in the same language that is the first message in the conversation.
+  Try to be brief and straight to the important points.`,
   hi: `Answer`
 };
